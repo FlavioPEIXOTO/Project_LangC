c87c3bc9a0a8abfca79d593a89cb3e57844ba17a
Flavio <flaviocabecas2@gmail.com>
2020-01-15 12:13:16 +0100

    Ecran d'accueil

---
+++ Projet/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    printf("\n \n \n \nMAIN MENU \n1. Create New Game \n2. Load Saved Game \n3. About \n4. Exit\n");
+
+    int choix;
+    char name[20];
+    scanf("%d", &choix);
+
+    if (choix == 1)
+    {
+        printf("Please enter your name\n");
+        scanf("%s", &name);
+        printf("You are in a forest surronded by trees %s\n ", name);
+    }
+
+    if (choix == 2)
+    {
+        printf("Chargement de votre sauvegarde\n");
+    }
+
+    if (choix == 3)
+    {
+        printf("Les règles\n");
+    }
+
+    if (choix == 4)
+    {
+        system("exit");
+    }
+    return 0;
+}
+
+int donjon()
+{
+
+}
+
+int player ()
+{
+
+}
+
