89bbe571da46b7365afc78a76f40dd3842125e6d
Flavio <flaviocabecas2@gmail.com>
2020-02-19 12:03:09 +0100

    création fichier coffre

---
+++ Projet/chest.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/*Première énigme : Là où se rassemblent les habitants du ciel se trouve un chêne marqué par les lettres "N-E"
+  Deuxième énigme : */
+
+/*int Chest()
+{
+    int alea_number;
+    srand(time(NULL));
+    alea_number = rand();
+    printf("%d",alea_number);
+}*/
