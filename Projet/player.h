a94e4d97ae0664390b38a5987365e9d75de3ae0b
Tom <tom.cousdikian@ynov.com>
2020-03-04 09:58:45 +0100

    move

--- Projet/player.h
+++ Projet/player.h
@@ -1,4 +1,11 @@
 // Deplacement et Combats
-void player (int (*tableau)[10]); //joueur et sa position
+void player (int tableau[10][10]); //joueur et sa position
 
-//int inventory()
+//Caracteristiques
+typedef struct perso perso;
+struct perso{
+int pv;
+int defense;
+int force;
+int lvl;
+};
