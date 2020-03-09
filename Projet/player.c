5fd18320dea9cd76ae509713f4f2788d807ae676
Tom <tom.cousdikian@ynov.com>
2020-03-09 14:32:33 +0100

    detection_des_ennemis

--- Projet/player.c
+++ Projet/player.c
@@ -2,93 +2,105 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "map.h"
+#include "ennemi.h"
+
 //#include "player.h"
 
 //int height = 10;
 //int width = 10;
 //int move;
+
 void player (int tableau[10][10], int height, int width, int move) //joueur et sa position
 {
     int x = 0;
     int y = 0;
-    for (x ; x < height ; x++)
+    int i,j;
+
+    for (i=0 ; i < height ; i++)
     {
-        for (y ; y < width ; y++)
+        for (j=0 ; j < width ; j++)
         {
-            printf("toto\n");
-            if (tableau[x][y] == 1)
-            printf("essaie\n");
+           // printf("toto\n");
+            if (tableau[i][j] == 1)
+            //printf("essaie\n");
             {
                 //printf("essaie\n");
+                x = i;
+                y = j;
 
-                printf("un nombre inferieur ou egal a 4\n");
-                printf("1 pour aller a l'Est\n");
-                printf("2 pour aller au Sud\n");
-                printf("3 pour aller au Nord\n");
-                printf("4 pour aller a l'Ouest\n");
-                scanf("%d", &move);
-                if (move == 1)
-                {
-                    printf("test\n");
-                    tableau[x][y] = 0;
-                    tableau[x][y+1] = 1;
-                    affiche(tableau, height, width);
-                    //printf("%ld", strlen(*tableau)[10]);
-                }
-                if (move == 2)
-                {
-                    printf("test2\n");
-                    tableau[x][y] = 0;
-                    tableau[x+1][y] = 1;
-                    affiche(tableau, height, width);
-                }
-                if (move == 3)
-                {
-                    printf("test3\n");
-                    tableau[x][y] = 0;
-                    tableau [x-1][y] = 1;
-                    affiche(tableau, height, width);
-                }
-                if (move == 4)
-                {
-                    printf("test4\n");
-                    tableau[x][y] = 0;
-                    tableau[x][y-1] = 1;
-                    affiche(tableau, height, width);
-                }
-                if (move == 7)
-                {
-                    printf("Ouverture de l'inventaire : \n");
-                    //inventory();
-                }
                 /*
                 if (move <=0 || move >4)
                 {
                     printf("nombre entre 1 et 4");
                     player((*tableau)[10],  height,  width);
                 }*/
-                //x++;
-                //y++;
             }
         //y++;
         }
     //x++;
     }
-    printf("test_final\n");
+
+    printf("un nombre inferieur ou egal a 4\n");
+    printf("1 pour aller a l'Est\n");
+    printf("2 pour aller au Sud\n");
+    printf("3 pour aller au Nord\n");
+    printf("4 pour aller a l'Ouest\n");
+    scanf("%d", &move);
+
+    if (move == 1)
+    {
+        printf("test\n");
+        if (tableau[x][y+1] == 2)
+        {
+            printf("lancement du combat\n");
+        }
+        tableau[x][y] = 0;
+        tableau[x][y+1] = 1;
+        affiche(tableau, height, width);
+        //printf("%ld", strlen(*tableau)[10]);
+    }
+    else if (move == 2)
+    {
+        printf("test2\n");
+        if(tableau[x+1][y] == 2 )
+        {
+            printf("lancement du combat\n");
+        }
+        tableau[x][y] = 0;
+        tableau[x+1][y] = 1;
+        affiche(tableau, height, width);
+    }
+    else if (move == 3)
+    {
+        printf("test3\n");
+        if(tableau[x-1][y] == 2 )
+        {
+            printf("lancement du combat\n");
+        }
+        tableau[x][y] = 0;
+        tableau [x-1][y] = 1;
+        affiche(tableau, height, width);
+    }
+    else if (move == 4)
+    {
+        printf("test4\n");
+        if(tableau[x][y-1] == 2 )
+        {
+            printf("lancement du combat\n");
+        }
+        tableau[x][y] = 0;
+        tableau[x][y-1] = 1;
+        affiche(tableau, height, width);
+    }
+    else if (move == 7)
+    {
+        printf("Ouverture de l'inventaire : \n");
+        //inventory();
+    }
+    else {
+            printf("test_final\n");
     //printf ("%d | ", tableau[x][y]);
+    }
+
 }
 
-/*
-int figth(){
-    //int pv = 500;
-    int run;
-    //scanf("%d", &run);
-    struct perso player;
-    player.pv = 100;
-    player.force = 15;
-    while (pv !=0 || run == 11);
-    {
-        printf("En combat");
-    }
-    return 0;
-}*/
