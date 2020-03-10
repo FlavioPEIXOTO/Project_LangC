a3901b8f5f304c4845c1b8099048fa99945d0739
ShadrackBodje13 <56391856+ShadrackBodje13@users.noreply.github.com>
2020-03-10 15:45:10 +0100

    update ennemi

--- Projet/ennemi.c
+++ Projet/ennemi.c
@@ -3,6 +3,15 @@
 #include "player.h" // là se trouve la structure personneage
 #include <time.h> // pour rand
 # include <string.h>
+struct Personnage {
+	int pv;
+	int attaque1;
+	int arme;
+	char name[100]; // pour stocker son nom
+};
+typedef struct Personnage Personnage; // du coup Personnage == struct Personnage
+
+
 
 // Je créer les valeurs ennemis (je ne sais pas si c'est necessaire de les mettre dans une fonction ou non ou les mettre dans la fonction combat
 
@@ -30,17 +39,18 @@ void affichageMonstre1(){
 	printf(" /n");
 }
 //Affichage Monstre 2
+
 void affichageMonstre2(){
-    printf("                 -==\\                         `//~\\   ~~~~`---.___.-~~");
-    printf("             ______-==|                         | |  \\           _-~`");
-    printf("       __--~~~  ,-/-==\\                        | |   `\        ,'");
-    printf("    _-~       /'    |  \\                      / /      \      /");
-    printf("  .'        /       |   \\                   /' /        \   /'");
-    printf(" /  ____  /         |    \`\.__/-~~ ~ \ _ _/'  /          \/'");
+    printf("                 -==\\\\                         `//~\\\\   ~~~~`---.___.-~~");
+    printf("             ______-==|                         | |  \\\\           _-~`");
+    printf("       __--~~~  ,-/-==\\\\                       | |   `\\        ,'");
+    printf("    _-~       /'    |  \\\\                      / /      \\      /");
+    printf("  .'        /       |   \\\\                   /' /        \\   /'");
+    printf(" /  ____  /         |    \\`\\.__/-~~ ~ \ _ _/'  /          \\/'");
     printf("/-'~    ~~~~~---__  |     ~-/~         ( )   /'        _--~`");
-    printf("                  \_|      /        _)   ;  ),   __--~~");
-    printf("                    '~~--_/      _-~/-  / \   '-~ \");
-    printf("                   {\__--_/}    / \\_>- )<__\      \");
+    printf("                  \\_|      /        _)   ;  ),   __--~~");
+    printf("                    '~~--_/      _-~/-  / \\   '-~ \\");
+    printf("                   {\\__--_/}    / \\\\_>- )<__\\      \\");
     printf("                   /'   (_/  _-~  | |__>--<__|      |");
     printf("                  |0  0 _/) )-~     | |__>--<__|     |");
     printf("                  / /~ ,_/       / /__>---<__/      |");
@@ -48,8 +58,8 @@ void affichageMonstre2(){
     printf("                 (^(~          /~_>---<__-      _-~");
     printf("                ,/|           /__>--<__/     _-~");
     printf("             ,//('(          |__>--<__|     /                  .----_");
-    printf("            ( ( '))          |__>--<__|    |                 /' _---_~\");
-    printf("         `-)) )) (           |__>--<__|    |               /'  /     ~\`\");
+    printf("            ( ( ))          |__>--<__|    |                 /' _---_~\\");
+    printf("         `-)) )) (           |__>--<__|    |               /'  /     ~\\`\\");
     printf("        ,/,'//( (             \__>--<__\    \            /'  //        ||");
     printf("      ,( ( ((, ))              ~-__>--<_~-_  ~--____---~' _/'/        /'");
     printf("    `~/  )` ) ,/|                 ~-_~>--<_/-__       __-~ _/");
@@ -62,32 +72,38 @@ void affichageMonstre2(){
 
 //Fonction de combat avec le monstre1
 
-int Combat1 (Personnage* Player, Personnage* Ennemi1){
+int Combat1 (Personnage *Player,Personnage*Ennemi1){
     //char alternative = "f"; //pour fuire
     //int tourEnnemi;
-    while (Player.pv == 0 || Ennemi1.pv==0){
+    Ennemi1->pv = 50;
+    Ennemi1->attaque1 = 10;
+    //Ennemi1->name = "Chat_masque";
+    while (Player->pv == 0 || Ennemi1->pv == 0){
         affichageMonstre1();
         printf("L'ennemi attaque");
-        Player.pv -= Ennemi1.attaque1; //on retranche dans les points de vie joueur, la force du monstre en gros j'appelle la valeur de player et de cette valeur je vais utiliser les points de vie
+        Player->pv -= Ennemi1->attaque1; //on retranche dans les points de vie joueur, la force du monstre en gros j'appelle la valeur de player et de cette valeur je vais utiliser les points de vie
         printf("Au joueur d'attaquer");
-        Ennemi1.pv -= player.attaque1;
+        Ennemi1->pv -= Player->attaque1;
 	}
-	if (Ennemi1.pv==0){ //si vie ennemie ==0 alors on retourne à la map avec les points players
-		printf("Ennemi1 mort")
+	if (Ennemi1->pv==0){ //si vie ennemie ==0 alors on retourne à la map avec les points players
+		printf("Ennemi1 mort");
 	}
 }
-int Combat2 (Personnage* Player, Personnage* Ennemi2){
+int Combat2 (Personnage *Player,Personnage *Ennemi2){
     //char alternative = "f"; //pour fuire
     //int tourEnnemi;
-    while(Player.pv == 0 || Ennemi2.pv)==0){
+    Ennemi2->pv = 90;
+    Ennemi2->attaque1 = 15;
+    //Ennemi2->name = "Omar_Simps";
+    while(Player->pv == 0 || Ennemi2->pv==0){
         affichageMonstre2();
         printf("L'ennemi attaque");
-        Player.pv -= Ennemi2.attaque1; //on retranche dans les points de vie joueur, la force du monstre en gros j'appelle la valeur de player et de cette valeur je vais utiliser les points de vie
+        Player->pv -= Ennemi2->attaque1; //on retranche dans les points de vie joueur, la force du monstre en gros j'appelle la valeur de player et de cette valeur je vais utiliser les points de vie
         printf("Au joueur d'attaquer");
-        Ennemi2.pv -= player.attaque1;
+        Ennemi2->pv -= Player->attaque1;
 	}
-	if (Ennemi2.pv)==0){ //si vie ennemie ==0 alors on retourne à la map avec les points players
-		printf("Ennemi2 mort")
+	if (Ennemi2->pv==0){ //si vie ennemie ==0 alors on retourne à la map avec les points players
+		printf("Ennemi2 mort");
 	}
 }
 
--- Projet/ennemi.h
+++ Projet/ennemi.h
@@ -1,3 +1,22 @@
+
+
+// structure de variable pour attributs personnages
+struct Personnage {
+	int pv;
+	int attaque1;
+	int arme;
+	char name[100]; // pour stocker son nom
+};
+
+
+struct Conversations {
+	char phrase1;
+	char phrase2;
+	char phrase3;
+	char phrase4;
+	char phrase5;
+};
+
 //typedef struct Object Personnage;
 /*
 void affichageMonstre1(Personnage personnage);
@@ -6,24 +25,8 @@ void Phrase ();
 */
 void affichageMonstre();
 // Deplacement et Combats
-void player (int (*tableau)[10]); //joueur et sa position
+void player (int tableau[10][10], int height, int width, int move);
+//void player (int (*tableau)[10]); //joueur et sa position
 typedef struct Personnage Personnage; // du coup Personnage == struct Personnage
 typedef struct Conversations Conversations; // du coup Conversations == struct Conversations
 //int inventory()
-
-// structure de variable pour attributs personnages
-struct Personnage {
-	int pv;
-	int attaque1;
-	int attaque2;
-	int attaque3;
-	int defense;
-	char name[100]; // pour stocker son nom
-};
-struct Conversations {
-	char phrase1;
-	char phrase2;
-	char phrase3;
-	char phrase4;
-	char phrase5;
-};
