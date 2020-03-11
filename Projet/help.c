97e626a1a30a657116c9f51a037e7069c4a2f05e
Tom <tom.cousdikian@ynov.com>
2020-03-11 17:10:24 +0100

    help

--- Projet/help.c
+++ Projet/help.c
@@ -13,22 +13,24 @@ void help ()
     scanf("%d", &help_choice);
     if (help_choice == 1)
     {
-        printf("Moves: \n ""east"" --> go to east \n ""north"" --> go to north \n ""west"" --> go to west \n ""south"" --> go to south \n \n ");
+        printf("Moves: \n ""1"" --> go to east \n ""3"" --> go to north \n ""4"" --> go to west \n ""2"" --> go to south \n \n ");
     }
 
     else if (help_choice == 2)
     {
-        printf("Inventory : \n ""inv"" --> open your inventory\n");
+        printf("Inventory : \n ""7"" --> open your inventory\n");
         show_inventory(tbl_obj);
 
     }
     else if (help_choice == 3)
     {
         printf("Rage quit : ""exit"" --> Please don't do it, don't leave our game :(\n");
+        system("exit");
     }
     else if (help_choice == 4)
     {
-        printf("Map \n");
+        printf("Map : \n");
+        printf("""7"" --> open your map\n");
     }
     else{
         help();
