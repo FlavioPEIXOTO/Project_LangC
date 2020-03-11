c58bf4ddb21e9151735c274c4249c74d1914371e
Tom <tom.cousdikian@ynov.com>
2020-03-11 17:03:34 +0100

    coffre_aleat

--- Projet/chest.c
+++ Projet/chest.c
@@ -11,30 +11,125 @@ int num;
 int nbr_life_pot = 0;
 //int nbr_life_pot = 0; Test d'ajout de potions de soin
 
-int printRandomsChest(int lower_chest, int upper_chest, int count_chest, int num)
+    //Random
+int printRandomsChest(int lower_chest, int upper_chest,
+                             int count_chest, int num)
 {
     int i;
     for (i = 0; i < count_chest; i++) {
         num = (rand() %
            (upper_chest - lower_chest + 1)) + lower_chest;
-           printf("%d\n",num);
+            //printf("%d \n",num);
+            //result();
+            if (num==1)
+            {
+                printf("potion de vie en +\n");
+            }
+            if (num==2)
+            {
+                printf("potion de force en +\n");
+            }
+            if (num==3)
+            {
+                printf("\n La ou se rassemblent les habitants du ciel, se cache un Etre de puissance a l Ouest de leurs positions \n");
+            }
+            if (num==4)
+            {
+                printf("armes en +\n");
+            }
+            if (num==5)
+            {
+                printf("\n Vous rencontrez un arbre, en faisant le tour vous observez une gravure \n Gravure : N-E \n");
+            }
     }
     return num;
 }
 
-int test()
+    //Borne Random
+int RndChest()
 {
-    int lower_chest = 1, upper_chest = 7, count_chest = 1;
+    int lower_chest = 1, upper_chest = 5, count_chest = 1;
 
     // Use current time as
     // seed for random generator
     srand(time(0));
 
     printRandomsChest(lower_chest, upper_chest, count_chest, num);
+    //result();
+    return 0;
+}
+
+
+        //Ancien Code
+/*
+int printRandomsEnigme(int lower_enigme, int upper_enigme,
+                             int count_enigme, int num)
+{
+    int i;
+    for (i = 0; i < count_enigme; i++) {
+        num = (rand() %
+           (upper_enigme - lower_enigme + 1)) + lower_enigme;
+    }
+
+    return num;
+}
+
+int RndEnigme()
+{
+    int lower_enigme = 1, upper_enigme = 2, count_enigme = 1;
+
+    // Use current time as
+    // seed for random generator
+    srand(time(0));
+
+    printRandomsEnigme(lower_enigme, upper_enigme, count_enigme, num);
 
     return 0;
 }
 
+void Enigme1()
+{
+    printf("\n Vous rencontrez un arbre, en faisant le tour vous observez une gravure \n Gravure : N-E \n");
+}
+
+void Enigme2()
+{
+    printf("\n Là où se rassemblent les habitants du ciel, se cache un être de puissance au Nord de leurs positions \n");
+}
+
+void DropEnigme(num)
+{
+    if (num == 1)
+    {
+        Enigme1();
+    }
+    else if (num == 2)
+    {
+        Enigme2();
+    }
+}*/
+/*
+void DropChest(int num)
+{
+    RndChest();
+    printf(num);
+    if (num == 1)
+    {
+        printf("+Potion de vie");
+    }
+    else if (num == 2)
+    {
+        printf("+Enigme");
+        RndEnigme(num);
+        DropEnigme();
+    }
+    else if (num == 3)
+    {
+        printf("+Arme");
+    }
+}*/
+
+/*
 int MorePotiondeSoin(int PotVie)
 {
     printf("%d",PotVie);
@@ -59,12 +154,13 @@ int LessPotiondeForce(int PotForce)
     PotForce--;
     return PotForce;
 }
-
+*/
+/*
 void Chest(int num){
 
     if (num == 1)
     {
-        /*ChestPotionDeSoin(nbr_life_pot);*/
+        //ChestPotionDeSoin(nbr_life_pot);
         MorePotiondeSoin(PotVie);
     }
     if (num == 2)
@@ -72,7 +168,32 @@ void Chest(int num){
         printf("toto");
     }
 }
-
+*/
+/*
+void result()
+{
+    RndChest();
+    if (num==1)
+    {
+        printf("potion de vie en +\n");
+    }
+    if (num==2)
+    {
+        printf("potion de force en +\n");
+    }
+    if (num==3)
+    {
+        printf("enigme en +\n");
+    }
+    if (num==4)
+    {
+        printf("armes en +\n");
+    }
+    if (num==5)
+    {
+        printf("\n Vous rencontrez un arbre, en faisant le tour vous observez une gravure \n Gravure : N-E \n");
+    }
+}*/
 
 /*int ChestPotionDeSoin(int nbr_life_pot)
 {
@@ -90,23 +211,3 @@ void Chest(int num){
     return nbr_life_pot;
 }*/
 //conservation des énigmes
-
-void Enigme()
-{
-    printf("Enigmes à venir");
-}
-
-void Enigme1()
-{
-    printf("Vous rencontrez un arbre, en faisant le tour vous observez une gravure \n Gravure : N-E");
-}
-
-void Enigme2()
-{
-    printf("Là où se rassemblent les habitants du ciel, se cache un être de puissance au Nord de leurs positions");
-}
-
-void Enigme3()
-{
-    printf("_");
-}
--- Projet/chest.h
+++ Projet/chest.h
@@ -1,8 +1,14 @@
 //intChest(void);
 int Chest(int num);
 
+void DropChest(int num);
+//void result(num);
+int RndChest();
+
+/*
 int MorePotiondeSoin(int PotVie);
 int MorePotiondeForce(int PotForce);
 int LessPotiondeSoin(int PotVie);
 int LessPotiondeForce(int PotForce);
 int test();
+*/
